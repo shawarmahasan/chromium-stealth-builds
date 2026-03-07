4dfadc388b035c9a396475f9365bcd283f463c6b
shawarmahasan <61228749+shawarmahasan@users.noreply.github.com>
2026-03-07 03:52:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3269,53 +3269,6 @@ client.on('channelDelete', channel => {
     })
 });
 
-client.on('channelUpdate', (oldChannel, newChannel) => {
-    if(!oldChannel.guild) return;
- 
-    var logChannel = oldChannel.guild.channels.find(c => c.name === 'log');
-    if(!logChannel) return;
- 
-    if(oldChannel.type === 'text') {
-        var channelType = 'Text';
-    }else
-    if(oldChannel.type === 'voice') {
-        var channelType = 'Voice';
-    }else
-    if(oldChannel.type === 'category') {
-        var channelType = 'Category';
-    }
- 
-    oldChannel.guild.fetchAuditLogs().then(logs => {
-        var userID = logs.entries.first().executor.id;
-        var userAvatar = logs.entries.first().executor.avatarURL;
- 
-        if(oldChannel.name !== newChannel.name) {
-            let newName = new Discord.RichEmbed()
-            .setTitle('**[قناة التحرير]**')
-            .setThumbnail(userAvatar)
-            .setColor('BLUE')
-            .setDescription(`**\n**:wrench: تم التعديل بنجاح **${channelType}** Channel Name\n\n**اسم قديم:** \`\`${oldChannel.name}\`\`\n**اسم جديد:** \`\`${newChannel.name}\`\`\n**معرف القناة:** ${oldChannel.id}\n**بواسطة:** <@${userID}> (ID: ${userID})`)
-            .setTimestamp()
-            .setFooter(oldChannel.guild.name, oldChannel.guild.iconURL)
- 
-            logChannel.send(newName);
-        }
-        if(oldChannel.topic !== newChannel.topic) {
-            let newTopic = new Discord.RichEmbed()
-            .setTitle('**[قناة التحرير]**')
-            .setThumbnail(userAvatar)
-            .setColor('BLUE')
-            .setDescription(`**\n**:wrench: Successfully Edited **${channelType}** Channel Topic\n\n**Old Topic:**\n\`\`\`${oldChannel.topic || 'NULL'}\`\`\`\n**New Topic:**\n\`\`\`${newChannel.topic || 'NULL'}\`\`\`\n**Channel:** ${oldChannel} (ID: ${oldChannel.id})\n**By:** <@${userID}> (ID: ${userID})`)
-            .setTimestamp()
-            .setFooter(oldChannel.guild.name, oldChannel.guild.iconURL)
- 
-            logChannel.send(newTopic);
-        }
-    })
-});
- 
- 
-// Guild Logs
 client.on('guildBanAdd', (guild, user) => {
  
     if(!guild.member(client.user).hasPermission('EMBED_LINKS')) return;
